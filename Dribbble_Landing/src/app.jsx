744ee0cf8650bfadc4a98c70c2d32df02b88b71c
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-09 12:26:56 -0500

    Created new MainTitle component

--- Dribbble_Landing/src/app.jsx
+++ Dribbble_Landing/src/app.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import { Header } from "./components/Header/Header.jsx"
 import { Badge } from "./components/Badge/Badge.jsx"
+import { MainTitle } from "./components/LargeTitle/MainTitle.jsx"
 
 export function App(){
     return(
       <>
       <Header/>
       <Badge bgColor="#ffda79"text="Over 3 million ready-to-work creatives!"/>
+      <MainTitle text='The world’s destination for design'/>
       </>
     )
   }
