1ab1b9b09b3db7aa8a9617fe484457934234c092
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-21 17:42:19 -0500

    Ajusted Footer component styles and content

--- Dribbble_Landing/src/components/Footer/Footer.jsx
+++ Dribbble_Landing/src/components/Footer/Footer.jsx
@@ -3,18 +3,30 @@ import { Logo } from '../Logo/Logo.jsx'
 import { PageLink } from '../PageLink/PageLink'
 
 export function Footer(){
+
     return(
-        <div className='footer-links-section'>
-            <Logo className='logo'/>
-            <div className='links-container'>
-                <PageLink title='For designers'/>
-                <PageLink title='Hire talent'/>
-                <PageLink title='Inspiration'/>
-                <PageLink title='Advertising'/>
-                <PageLink title='Blog'/>
-                <PageLink title='About'/>
-                <PageLink title='Careers'/>
-                <PageLink title='Support'/>
+        <div className='footer-section'>
+            <div className='footer-links-section'>
+                <Logo className='logo'/>
+                <div className='links-container'>
+                    <PageLink title='For designers'/>
+                    <PageLink title='Hire talent'/>
+                    <PageLink title='Inspiration'/>
+                    <PageLink title='Advertising'/>
+                    <PageLink title='Blog'/>
+                    <PageLink title='About'/>
+                    <PageLink title='Careers'/>
+                    <PageLink title='Support'/>
+                </div>
+                {// ToDo: Replace icon for svg 
+                // ? How to add local svg file
+                }
+                <div className='social-media-container'>
+                    <div className='icon'></div>
+                    <div className='icon'></div>
+                    <div className='icon'></div>
+                    <div className='icon'></div>
+                </div>
             </div>
         </div>
     )
