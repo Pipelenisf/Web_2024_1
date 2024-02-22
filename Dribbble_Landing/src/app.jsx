545a2a3adc8e9f72917319838d5fda9437114f22
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-22 18:31:50 -0500

    feat: added map to productCard

--- Dribbble_Landing/src/app.jsx
+++ Dribbble_Landing/src/app.jsx
@@ -14,15 +14,13 @@ import { Footer } from "./components/Footer/Footer.jsx"
 export function App(){
     return(
       <>
-      {// ToDo: ¡URGENT! Fixed svg size
-      }
       <Header/>
       <Badge bgColor="#ffda79"text="Over 3 million ready-to-work creatives!"/>
       <MainTitle text='The world’s destination for design'/>
       <BodyContainer text='Get inspired by the work of millions of top-rated designers & agencies around the world.'/>
       <MainButton title='Get Started' bgColor='#0d0c22' txtColor='white'/>
       <UserCard bgimage='https://img.freepik.com/foto-gratis/pintura-lago-montana-montana-al-fondo_188544-9126.jpg'/>
-      <ProductCard bgProductImg='https://cdn.dribbble.com/userupload/13006921/file/original-562923298283ee2a026cb7fc94ab0396.png?resize=1200x900'/>
+      <ProductCard/>
       <CategoryCard text='Typefaces'/>
       <Footer/>
       </>
