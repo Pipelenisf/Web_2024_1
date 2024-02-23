70308316872679d25d921cbebba5213a387cd85e
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-23 08:20:20 -0500

    styles: implemented css grid to ProductCard com

--- Dribbble_Landing/src/const/ProductData.js
+++ Dribbble_Landing/src/const/ProductData.js
@@ -23,5 +23,29 @@ export const PRODUCT = [
         views:'250k',
         accountImage:'https://cdn.dribbble.com/users/230124/avatars/small/999ed09dc97f840f7aef7347217b1147.jpg?1483316271',
     },
+    {
+        id: crypto.randomUUID(),
+        image:'https://cdn.dribbble.com/userupload/13006921/file/original-562923298283ee2a026cb7fc94ab0396.png?resize=1200x900',
+        userName:'Alex Krugli',
+        likes:'156',
+        views:'250k',
+        accountImage:'https://cdn.dribbble.com/users/230124/avatars/small/999ed09dc97f840f7aef7347217b1147.jpg?1483316271',
+    },
+    {
+        id: crypto.randomUUID(),
+        image:'https://cdn.dribbble.com/userupload/13006921/file/original-562923298283ee2a026cb7fc94ab0396.png?resize=1200x900',
+        userName:'Alex Krugli',
+        likes:'156',
+        views:'250k',
+        accountImage:'https://cdn.dribbble.com/users/230124/avatars/small/999ed09dc97f840f7aef7347217b1147.jpg?1483316271',
+    },
+    {
+        id: crypto.randomUUID(),
+        image:'https://cdn.dribbble.com/userupload/13006921/file/original-562923298283ee2a026cb7fc94ab0396.png?resize=1200x900',
+        userName:'Alex Krugli',
+        likes:'156',
+        views:'250k',
+        accountImage:'https://cdn.dribbble.com/users/230124/avatars/small/999ed09dc97f840f7aef7347217b1147.jpg?1483316271',
+    },
     
 ]
