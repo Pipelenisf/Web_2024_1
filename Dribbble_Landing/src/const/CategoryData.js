63b1058a0de2fbf6864f2e7e231852640d97c365
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-26 17:20:44 -0500

    docc: changed CategoryCard data content

--- Dribbble_Landing/src/const/CategoryData.js
+++ Dribbble_Landing/src/const/CategoryData.js
@@ -6,18 +6,33 @@ export const CATEGORY = [
     },
     {
         id: crypto.randomUUID(),
-        bgImage: 'https://cdn.dribbble.com/userupload/13179360/file/original-e3c15efe4abdd60484f3d90913d2a788.png',
+        bgImage: 'https://cdn.dribbble.com/userupload/13154060/file/original-7ba817964c78295970688d3213480875.jpg?format=webp&resize=273x205&vertical=center',
+        text: 'Illustration'
+    },
+    {
+        id: crypto.randomUUID(),
+        bgImage: 'https://cdn.dribbble.com/userupload/13208827/file/original-1ab4dd91a92157499b339447ba87bacb.png?format=webp&resize=273x205&vertical=center',
+        text: 'Branding'
+    },
+    {
+        id: crypto.randomUUID(),
+        bgImage: 'https://cdn.dribbble.com/userupload/12655802/file/still-8296ebd00519848b9931c999d587b424.png?format=webp&resize=273x205&vertical=center',
+        text: 'Animation'
+    },
+    {
+        id: crypto.randomUUID(),
+        bgImage: 'https://cdn.dribbble.com/userupload/13194408/file/original-8ffde1101c1e637ebefbba4f23902192.jpg?format=webp&resize=273x205&vertical=center',
+        text: 'Product Design'
+    },
+    {
+        id: crypto.randomUUID(),
+        bgImage: 'https://cdn.dribbble.com/userupload/13191814/file/still-da089127684e2273d7eb31c9d32a54ae.png?format=webp&resize=273x205&vertical=center',
         text: 'Mobile'
     },
     {
         id: crypto.randomUUID(),
-        bgImage: 'https://cdn.dribbble.com/userupload/13179360/file/original-e3c15efe4abdd60484f3d90913d2a788.png',
-        text: 'Mobile'
-    },
-    {
-        id: crypto.randomUUID(),
-        bgImage: 'https://cdn.dribbble.com/userupload/13179360/file/original-e3c15efe4abdd60484f3d90913d2a788.png',
-        text: 'Mobile'
+        bgImage: 'https://cdn.dribbble.com/userupload/13010215/file/original-c890290302becd800649b2f4e4a07b68.png?format=webp&resize=273x205&vertical=center',
+        text: 'Print'
     },
     
 ]
