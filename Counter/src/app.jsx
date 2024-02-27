b43cf1b863ad4cd46fba59b7fb8537120735ba67
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-26 23:20:09 -0500

    feat: added showGif function

--- Counter/src/app.jsx
+++ Counter/src/app.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react"
+import React, { useState, useEffect} from "react"
 import { Button } from "./components/Button/Button"
 
 export function App() {
 
   const [value, setValue] = useState(0)
+  const [showGif, setShowGif] = useState(false)
 
   const addValue = () => {
     setValue(value + 1)
+    setShowGif(true)
   }
 
   const substractValue = () => {
@@ -15,9 +17,26 @@ export function App() {
     : null
   }
 
+  useEffect(() => {
+    console.log('Dead drop')
+    if (showGif) {
+      const timer = setTimeout(() => {
+        setShowGif(false)
+      }, 1000)
+
+      return () => clearTimeout(timer)
+    }
+  }, [showGif])
+
+
   return (
     <>
       <p>{value}</p>
+        {showGif 
+        ? <img src="https://media0.giphy.com/media/gFP6NLJdiePZ60QtLN/200w.gif" 
+               alt="gif" />
+        : null
+        }
       <div>
         <button onClick={substractValue} text='-' bgColor='green'/>
         <button onClick={addValue} text='+' bgColor='red'/>
