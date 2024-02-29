1ba369675848c7503154e2b34ce771e89b281e26
Pipelenisf <98329921+Pipelenisf@users.noreply.github.com>
2024-02-29 18:45:08 -0500

    Created new Form component

---
+++ Todo_List/src/Components/Form/Form.jsx
@@ -0,0 +1,18 @@
+import './Form.css'
+
+export function Form(){
+    return(
+        <form>
+            <input type="label">
+            </input>
+            
+            <Button/>
+        </form>
+    )
+}
+
+export function Button(){
+    return(
+        <button>+</button>
+    )
+}
